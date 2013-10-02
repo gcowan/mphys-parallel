e753e9863d49c2b108f1ae20d328448069c87162
Mark Stringer <markstringer92@gmail.com>
2013-10-02 13:20:53 +0100

    Simple fit code parallelised runs around 3.5 times faster

--- testFit/negativeLog.cpp
+++ testFit/negativeLog.cpp
@@ -9,20 +9,25 @@ double negativeLog::evaluateDataSetParallel(double * data, int dataLength, doubl
     //summing up all the logs
     int numThreads = 8;
     double total [numThreads];
-    #pragma omp parallel  default(none) shared(total,numThreads,normalization,dataLength,data,func)
+    //ensuring all values in total are set to 0
+    for(int i=0; i<numThreads; i++){
+        total[i] = 0;
+    }
+    #pragma omp parallel  default(none) shared(total,numThreads,normalization,dataLength,data,sigmaVal) 
     {
         int threadNum = omp_get_thread_num();
+        gaussianFunc threadFunc = gaussianFunc(sigmaVal);
         int lowerBound = (dataLength/numThreads)*(threadNum);
         int upperBound =  (dataLength/numThreads)*(threadNum+1);
 
-        for(int i =lowerBound; i<upperBound; i++){
-            double val = func.evaluate(data[i]);
+        for(int i = lowerBound; i<upperBound; i++){
+            double val = threadFunc.evaluate(data[i]);
             total[threadNum] += log(val/normalization);
         }
         //Making sure we iterate upto the last data element
         if(threadNum==numThreads-1 && upperBound<dataLength){
             for(int i=upperBound; i<dataLength; i++){
-                double val = func.evaluate(data[i]);
+                double val = threadFunc.evaluate(data[i]);
                 total[threadNum] += log(val/normalization);
             }
         }
@@ -44,7 +49,7 @@ double negativeLog::evaluateDataSetSerial(double * data, int dataLength, double 
     //summing up all the logs
     for(int i =0; i<dataLength; i++){
         double val = func.evaluate(data[i]);
-        total[threadNum] += log(val/normalization);
+        total += log(val/normalization);
     }
     
     return -total;
